51ccbf4ae7603bb20fdb6ae5a5d3f5bdd4f998bb
Payton <payton@foolord.com>
2012-11-11 14:43:29 -0500

    Add styles for login and organize styles.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, :notice => "You are now logged in!"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now[:error] = "Invalid email or password."
       render "new"
     end
   end
