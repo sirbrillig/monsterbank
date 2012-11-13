a23cb806655be86f075b666b2e3c24fc5e4044e5
Payton Swick <payton@foolord.com>
2012-11-13 11:53:11 -0500

    Add stealth sign-up form for saving monster

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,30 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+
+    if params[:monid]
+      @monster = Monster.find(params[:monid])
+      @user.monsters << @monster
+    end
+
     if @user.save
-      redirect_to root_url, :notice => "You are signed-up!"
+      session[:user_id] = @user.id
+      if @monster
+        redirect_to monster_path(@monster), :notice => "Your monster has been saved!"
+      else
+        redirect_to root_url, :notice => "You are signed-up!"
+      end
     else 
-      render "new"
+      if @monster
+        render "save_monster"
+      else
+        render "new"
+      end
     end
   end
 
   def save_monster
     @user = User.new(params[:user])
-    @user.password = 'foobar' #FIXME: ugh, hack. Ask the user for a password somehow.
-    @user.password_confirmation = @user.password
     @monster = Monster.find(params[:monid])
-    @user.monsters << @monster
-    if @user.save
-      redirect_to monster_path(@monster), :notice => "Your monster is saved!"
-    else 
-      redirect_to monster_path(@monster), :notice => "There was a problem with your email address: #{@user.errors.full_messages.join(', ')}"
-    end
   end
 end
