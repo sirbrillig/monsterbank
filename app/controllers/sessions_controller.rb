0a26c256c9942f77e87c543aee244feb3d459e3c
Payton Swick <payton@foolord.com>
2012-11-29 19:53:49 -0500

    Add big New Monster button to home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "You are now logged in!"
+      redirect_to monsters_path, :notice => "You are now logged in!"
     else
       flash.now[:error] = "Invalid email or password."
       render "new"
